4d75edbe5413ce081fa9f88b8916e30c1c86bf71
Mario Arturo Sanchez Ruelas <marioarturosanchez@gmail.com>
2022-11-12 22:05:42 -0600

    Avast command fix

--- commands/😜 Fun/avast.js
+++ commands/😜 Fun/avast.js
@@ -3,6 +3,6 @@ name: "avast",
     aliases: [`antivirus`, `virus`],
     desc: "The virus database has been updated",
     run: async(client, message, args, prefix) => {
-        message.channel.send({files: ["/src/Avast Audio.mp3"]})
+        message.channel.send({files: [`${process.cwd()}/src/Avast Audio.mp3`]})
     }
 }    
